13f9f6d551ba7c82d5b3f69d2276be84353bf0a6
vasilii <porokh.vs@gmail.com>
2023-07-30 13:04:59 +0300

    process

--- src/index.js
+++ src/index.js
@@ -25,57 +25,73 @@ async function runApplication () {
     studio.addToScene(walkObject)
     walkObject.setMeshesToWalk(house.arrMeshesToWalk)
 
-    const ui = createUi()
     let currentCamera = 'orbit'
-    const changeCamera = () => {
-        if (currentCamera === 'orbit') {
+    studio.setCamera(cameraOrbit)
+
+    const ui = createUi()
+
+    ui.createButton(
+        'walk',
+        () => {
             studio.setCamera(walkObject.camera)
             walkObject.toggleActive(true)
             currentCamera = 'walk'
-        } else {
+        },
+        () => {
             studio.setCamera(cameraOrbit)
             walkObject.toggleActive(false)
             currentCamera = 'orbit'
         }
-    }
-    ui.setOnClick('change camera', changeCamera)
-    studio.setCamera(cameraOrbit)
+    )
 
-    let isShowFirstFloor = false
-    let isShowSecondFloor = false
-
-    ui.setOnClick('1-й этаж', button => {
-        ui.clear()
-        if (isShowFirstFloor) {
-            house.toggleVisible('1_', true)
-            house.toggleVisible('2_', true, true)
-            house.toggleVisible('3_', true, true)
-        } else {
-            button.style.backgroundColor = '#FFFF00'
-            isShowSecondFloor = false
+    ui.createButton(
+        'этаж 1',
+        () => {
+            ui.clearButtonsToggled(['этаж 2'])
             house.toggleVisible('1_', true)
             house.toggleVisible('2_', false, true)
             house.toggleVisible('3_', false, true)
-        }
-        isShowFirstFloor = !isShowFirstFloor
-    })
-
-
-    ui.setOnClick('2-й этаж', button => {
-        ui.clear()
-        if (isShowSecondFloor) {
+        },
+        () => {
+            ui.clearButtonsToggled(['этаж 2'])
             house.toggleVisible('1_', true)
             house.toggleVisible('2_', true)
             house.toggleVisible('3_', true)
-        } else {
-            button.style.backgroundColor = '#FFFF00'
-            isShowFirstFloor = false
-            house.toggleVisible('1_', false)
+        },
+    )
+
+    ui.createButton(
+        'этаж 2',
+        () => {
+            ui.clearButtonsToggled(['этаж 1'])
+            house.toggleVisible('1_', false, true)
             house.toggleVisible('2_', true)
-            house.toggleVisible('3_', false)
-        }
-        isShowSecondFloor = !isShowSecondFloor
-    })
+            house.toggleVisible('3_', false, true)
+        },
+        () => {
+            ui.clearButtonsToggled(['этаж 1'])
+            house.toggleVisible('1_', true)
+            house.toggleVisible('2_', true)
+            house.toggleVisible('3_', true)
+        },
+    )
+
+
+    // ui.setOnClick('2-й этаж', button => {
+    //     ui.clear()
+    //     if (isShowSecondFloor) {
+    //         house.toggleVisible('1_', true)
+    //         house.toggleVisible('2_', true)
+    //         house.toggleVisible('3_', true)
+    //     } else {
+    //         button.style.backgroundColor = '#FFFF00'
+    //         isShowFirstFloor = false
+    //         house.toggleVisible('1_', false)
+    //         house.toggleVisible('2_', true)
+    //         house.toggleVisible('3_', false)
+    //     }
+    //     isShowSecondFloor = !isShowSecondFloor
+    // })
 
     const animate = () => {
         requestAnimationFrame( animate )
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,23 +1,44 @@
 export const createUi = () => {
     const wrapper = document.createElement('div')
     wrapper.classList.add('ui-wrapper')
+
+    const disable = e => {
+        e.stopPropagation()
+        e.preventDefault()
+    }
+
+    wrapper.addEventListener('pointerdown', disable)
+    wrapper.addEventListener('pointerup', disable)
+    wrapper.addEventListener('pointermove', disable)
+    wrapper.addEventListener('mousemove', disable)
     document.body.appendChild(wrapper)
 
     const buttons = {}
 
-
     return {
-        setOnClick: (key, f) => {
+        createButton: (key, fOn, fOff) => {
             const b = document.createElement('button')
             b.classList.add('button-create')
             b.innerText = key
             wrapper.appendChild(b)
-            buttons[key] = b
-            buttons[key].addEventListener('click', () => f(b))
+            buttons[key] = {
+                isToggled: false,
+                button: b,
+            }
+            b.addEventListener('click', e => {
+                buttons[key].isToggled = !buttons[key].isToggled
+                b.style.backgroundColor = buttons[key].isToggled ? '#ffff00' : '#ffffff'
+                buttons[key].isToggled ? fOn() : fOff()
+            })
         },
-        clear: () => {
-            for (let key in buttons) {
-                buttons[key].style.backgroundColor = '#ffffff'
+        clearButtonsToggled: arr => {
+            for (let i = 0; i < arr.length; ++i) {
+                const key = arr[i]
+                if (!buttons[key]) {
+                    continue;
+                }
+                buttons[key].button.style.backgroundColor = '#ffffff'
+                buttons[key].isToggled = false
             }
         }
     }
